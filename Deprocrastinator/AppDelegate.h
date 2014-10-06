846026eeefe27adf73e5387eb51eef2e902f8cb1
Ryan Siska <rsiska1@gmail.com>
2014-10-06 13:37:39 -0500

    Initial Commit

---
+++ Deprocrastinator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Deprocrastinator
+//
+//  Created by S on 10/6/14.
+//  Copyright (c) 2014 Ryan Siska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
